c0ef2ffa78bcb50766fcf828577609913da6881b
minhng97 <minh.nguyentrong97@gmail.com>
2020-01-16 21:10:47 +0700

    Add classname module for todoitems

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
-
+import './TodoItem.css';
+var classname = require('classname');
 class TodoItem extends Component {
     render() {
+        const {item} = this.props;
+        let className = classname({
+            'TodoItem': true,
+            'TodoItem-complete': item.isComplete
+        });
+
         return (
-            <div className="TodoItem">
-                <p>{ this.props.title }</p>
+            <div className={className}>
+                <p> { item.title } </p>
             </div>
         )
     }
