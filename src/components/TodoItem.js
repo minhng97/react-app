291ca41beb433bfbfb510f7c3ad39812331477cc
minhng97 <minh.nguyentrong97@gmail.com>
2020-01-16 18:29:26 +0700

    Add: TodoItem

---
+++ src/components/TodoItem.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class TodoItem extends Component {
+    render() {
+        return (
+            <div className="TodoItem">
+                <p>{ this.props.title }</p>
+            </div>
+        )
+    }
+}
+
+
+export default TodoItem;
