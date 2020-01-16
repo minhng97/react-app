8342f332b281c0fde57f50b954f0c7faead8a9b4
minhng97 <minh.nguyentrong97@gmail.com>
2020-01-16 18:29:01 +0700

    Add: constructor for App class to render a list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TodoItem from './components/TodoItem';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.todoItems = [
+      'Go shopping', 'Eat dinner', 'Go to sleep'
+    ];
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        
+          {
+            this.todoItems.map((item, index) => (
+              <TodoItem key={index} title={item} />
+    ))
+          }
+
+  </header>
+      </div>
+    );
+  }
 }
+// function App() {
+ 
+// }
 
 export default App;
