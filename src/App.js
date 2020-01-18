b60404cfd7e2401a4a17abee40cf60e909d769ce
minhng97 <minh.nguyentrong97@gmail.com>
2020-01-18 17:54:06 +0700

    Add function onItemClick()

--- src/App.js
+++ src/App.js
@@ -5,17 +5,35 @@ import TodoItem from './components/TodoItem';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.todoItems = [
-      {title: 'Go shopping', isComplete: false}, 
-      {title: 'Eat dinner', isComplete: true},
-      {title: 'Go to sleep', isComplete: true}
-    ];
+    this.state = {
+      todoItems: [
+        {title: 'Go shopping', isComplete: false}, 
+        {title: 'Eat dinner', isComplete: true},
+        {title: 'Go to sleep', isComplete: true}
+      ]
+    }
+
+  }
+
+  onItemClicked(index) {
+    this.setState(state => {
+      const todoItems = state.todoItems;
+      let itemToChange = todoItems[index]
+      itemToChange.isComplete = !itemToChange.isComplete
+      todoItems[index] = itemToChange
+      
+      console.log(this.state.todoItems[index])
+      return {
+        todoItems
+      }
+  });
+  
   }
   render() {
     return (
       <div className="App">
-        { this.todoItems.length > 0
-          ? this.todoItems.map((item, index) => <TodoItem key={index} item={item} />)
+        { this.state.todoItems.length > 0
+          ? this.state.todoItems.map((item, index) => <TodoItem key={index} item={item} onClick={this.onItemClicked.bind(this, index)} />)
           : "There is nothing here"
         }
 
@@ -23,8 +41,6 @@ class App extends React.Component {
     );
   }
 }
-// function App() {
- 
-// }
+
 
 export default App;
