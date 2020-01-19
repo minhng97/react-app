492f80106a39d211f4466417b6948a8113d97946
minhng97 <minh.nguyentrong97@gmail.com>
2020-01-19 10:31:01 +0700

    Using slice for setState

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ class App extends React.Component {
     super(props);
     this.state = {
       todoItems: [
-        {title: 'Go shopping', isComplete: false}, 
-        {title: 'Eat dinner', isComplete: true},
-        {title: 'Go to sleep', isComplete: true}
+        { title: 'Go shopping', isComplete: false },
+        { title: 'Eat dinner', isComplete: true },
+        { title: 'Go to sleep', isComplete: true }
       ]
     }
 
@@ -18,30 +18,27 @@ class App extends React.Component {
   onItemClicked(item) {
     return () => {
       const isComplete = item.isComplete;
-      const {todoItems} = this.state;
+      const { todoItems } = this.state;
       const index = todoItems.indexOf(item);
-      
-      this.setState(state => {
-        console.log(item)
-        return {
-          todoItems: [
-            ...todoItems.slice(0, index),
-            {
-              ...item, isComplete: !isComplete
-            },
-            ...todoItems.slice(index + 1)
-          ]
-        }
-    });
-  
+
+      this.setState({
+        todoItems: [
+          ...todoItems.slice(0, index),
+          {
+            ...item, isComplete: !isComplete
+          },
+          ...todoItems.slice(index + 1)
+        ]
+      });
     }
-  // console.log(this.state.todoItems)
+
   }
   render() {
     return (
       <div className="App">
-        { this.state.todoItems.length > 0
-          ? this.state.todoItems.map((item, index) => <TodoItem key={index} item={item} onClick={this.onItemClicked(item)} />)
+        {this.state.todoItems.length > 0
+          ? this.state.todoItems.map((item, index) =>
+           <TodoItem key={index} item={item} onClick={this.onItemClicked(item)} />)
           : "There is nothing here"
         }
 
