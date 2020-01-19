40640da82ccb6990387c028eb4990c1a2a8194d7
minhng97 <minh.nguyentrong97@gmail.com>
2020-01-19 18:34:58 +0700

    Add function: add new Item

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import TodoItem from './components/TodoItem';
+import tick from './img/tick.svg';
 
 class App extends React.Component {
   constructor(props) {
@@ -13,6 +14,7 @@ class App extends React.Component {
       ]
     }
 
+    this.onKeyUp = this.onKeyUp.bind(this);
   }
 
   onItemClicked(item) {
@@ -32,13 +34,37 @@ class App extends React.Component {
       });
     }
 
+
   }
+  onKeyUp(event) {
+    if (event.keyCode === 13) { // ENTER key
+      let text = event.target.value;
+
+      if (!text) { return };
+
+      text = text.trim();
+      if (!text) { return };
+
+      this.setState({
+        todoItems: [
+          { title: text, isComplete: false },
+          ...this.state.todoItems ]
+      })
+
+      event.target.value ='';
+    }
+  }
+
   render() {
     return (
       <div className="App">
+        <div className="Header">
+          <img src={tick} width="32" height="32" alt="create" onClick={() => this.onKeyUp} />
+          <input type="text" placeholder="Add a new item" onKeyUp={this.onKeyUp} />
+        </div>
         {this.state.todoItems.length > 0
           ? this.state.todoItems.map((item, index) =>
-           <TodoItem key={index} item={item} onClick={this.onItemClicked(item)} />)
+            <TodoItem key={index} item={item} onClick={this.onItemClicked(item)} />)
           : "There is nothing here"
         }
 
