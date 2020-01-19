7cc66bd0fa5dcf4435ab9cdc4cd786c280d4e0a0
minhng97 <minh.nguyentrong97@gmail.com>
2020-01-19 22:35:09 +0700

    Add checkAll button

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import TodoItem from './components/TodoItem';
 import tick from './img/tick.svg';
 
+const ACTIVE = 'active';
+const COMPLETED = 'completed';
+const ENTER_KEY = 13;
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -16,11 +20,12 @@ class App extends React.Component {
 
     this.onKeyUp = this.onKeyUp.bind(this);
     this.changeFilter = this.changeFilter.bind(this);
+    this.checkAll = this.checkAll.bind(this);
   }
 
   onItemClicked(item) {
     return () => {
-      const isComplete = item.isComplete;
+      const { isComplete } = item;
       const { todoItems } = this.state;
       const index = todoItems.indexOf(item);
 
@@ -35,14 +40,10 @@ class App extends React.Component {
         ]
       });
     }
-
-
   }
   onKeyUp(event) {
-    if (event.keyCode === 13) { // ENTER key
+    if (event.keyCode === ENTER_KEY) {
       let text = event.target.value;
-
-      if (!text) { return };
 
       text = text.trim();
       if (!text) { return };
@@ -50,10 +51,10 @@ class App extends React.Component {
       this.setState({
         todoItems: [
           { title: text, isComplete: false },
-          ...this.state.todoItems ]
+          ...this.state.todoItems]
       })
 
-      event.target.value ='';
+      event.target.value = '';
     }
   }
 
@@ -63,25 +64,61 @@ class App extends React.Component {
     })
   }
 
-  render() {
-    let { todoItems, currentFilter } = this.state; 
+  checkAll() {
+    let { todoItems } = this.state;
+    const COMPLETED = true;
 
-    if (currentFilter === 'active') {
-      todoItems = todoItems.filter(item => item.isComplete === false)
-    } else if (currentFilter === 'completed') {
-      todoItems = todoItems.filter(item => item.isComplete === true)
+    let check = todoItems.every(item => item.isComplete === COMPLETED);
+    switch (check) {
+      case (true):
+        todoItems = todoItems.map(item => item = Object.assign({}, item, { isComplete: false }))
+        break;
+      default:
+        todoItems = todoItems.map(item => item = Object.assign({}, item, { isComplete: true }));
+    }
+
+    this.setState({
+      todoItems: [
+        ...todoItems
+      ]
+    })
+  }
+
+  render() {
+    let { todoItems, currentFilter } = this.state;
+
+    switch (currentFilter) {
+      case (ACTIVE):
+        todoItems = todoItems.filter(item => item.isComplete === false);
+        break;
+      case (COMPLETED):
+        todoItems = todoItems.filter(item => item.isComplete === true);
+        break;
+      default:
     }
 
     return (
       <div className="App">
         <div className="Header">
-          <img src={tick} width="32" height="32" alt="create" onClick={() => this.onKeyUp} />
-          <input type="text" placeholder="Add a new item" onKeyUp={this.onKeyUp} />
+
+          <img src={tick} width="32" height="32"
+            alt="create"
+            onClick={this.checkAll}
+          />
+
+          <input type="text"
+            placeholder="Add a new item"
+            onKeyUp={this.onKeyUp}
+          />
+
         </div>
 
-        <input type="button" onClick ={() => this.changeFilter('all')} value="all" />
-        <input type="button" onClick ={() => this.changeFilter('active')} value="active" />
-        <input type="button" onClick ={() => this.changeFilter('completed')} value="completed" />
+        <input type="button"
+          onClick={() => this.changeFilter('all')} value="all" />
+        <input type="button"
+          onClick={() => this.changeFilter('active')} value="active" />
+        <input type="button"
+          onClick={() => this.changeFilter('completed')} value="completed" />
 
         {todoItems.length > 0
           ? todoItems.map((item, index) =>
