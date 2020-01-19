dc694892acccf51cf9e3fddf46f4d4d1e653a5e7
minhng97 <minh.nguyentrong97@gmail.com>
2020-01-19 11:26:58 +0700

    Add img check for item

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,23 @@
 import React, {Component} from 'react';
 import './TodoItem.css';
+import checkImg from '../img/check.svg';
+import checkCompleteImg from '../img/check-complete.svg';
 var classname = require('classname');
+
+
 class TodoItem extends Component {
     render() {
         const { item, onClick } = this.props;
+        let url = checkImg;
+        if (item.isComplete) { url = checkCompleteImg };
         let className = classname({
             'TodoItem': true,
             'TodoItem-complete': item.isComplete
         });
 
         return (
-            <div onClick={onClick} className={className}>
+            <div className={className}>
+                <img onClick={onClick} src={url} alt="checkimg" width="32" height="32" />
                 <p> { item.title } </p>
             </div>
         )
